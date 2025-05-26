d4bccd139da3090766221ec36b9d00399ece1da1
pjusto <pjusto@cge.rj.gov.br>
2025-05-26 18:35:37 -0300

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,33 @@ function App() {
       <Header />
       <div className='fundo'>
       <div className="container" >
-          <h1 style={{ fontFamily: 'Pirata One, cursive', fontSize: '2.5rem' }}>
+          <h1 className='titulo1' style={{ fontFamily: 'Pirata One, cursive', fontSize: '2.5rem', marginTop: '150px' }}>
             Blackbeard Wiki
           </h1>
       </div>
       </div>
      <div className='fundo2'>
-      <div className='contribuidores'>
-        <h1 style={{fontFamily: 'Pirate One, cursive', fontSize: '2.5rem'}}>
-          Contribuidores
-        </h1>
+    <div className="container">
+  <h1 style={{ fontFamily: 'Pirata One, cursive', fontSize: '2.5rem', marginTop: '50px' }}>
+    Contribuidores
+  </h1>
+
+  <div className="cards">
+    {[
+      { nome: 'zKenay', img: '/contribuidores/zkenay.png' },
+      { nome: 'Zheryon', img: '/contribuidores/zheryon.png' },
+      { nome: 'zRyu', img: '/contribuidores/zryu.png' },
+      { nome: 'Pedrox', img: '/contribuidores/pedrox.png' },
+      { nome: 'Vitonezo', img: '/contribuidores/vitonezo.png' }
+    ].map((contribuidor, index) => (
+      <div className="card-contribuidor" key={index}>
+        <img src={contribuidor.img} alt={contribuidor.nome} />
+        <p>{contribuidor.nome}</p>
       </div>
+    ))}
+  </div>
+</div>
+
     </div>
     </>
   );
